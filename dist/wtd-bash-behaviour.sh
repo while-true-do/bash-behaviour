ed7597065e92f9d09c4ebe266f7b39c71df51c54
daniel-wtd <daniel@while-true-do.org>
2016-10-14 22:52:47 +0200

    More shopts and aliases

--- dist/wtd-bash-behaviour.sh
+++ dist/wtd-bash-behaviour.sh
@@ -2,14 +2,31 @@
 # While-True-Do: Bash-Behaviour
 # Some Adjustments for Bash, to provide a better behaviour.
 
+
+####################
+# binds            #
+####################
 ## Ignore Case for Tab Completion
 bind "set completion-ignore-case on"
 ## Scroll through tab completion
 # bind "TAB":"menu-complete"
 
-## cd without cd
-shopt -s autocd
+####################
+# shopts           #
+####################
+shopt -s autocd        # cd without cd
+shopt -s checkwinsize  # check window size and resize
+shopt -s cdspell       # autocorrect minor spelling errors
+shopt -s dirspell      # bash attempts spelling correction
+shopt -s dotglob       # include leading dot-files/dirs
 
+####################
+# Aliases          #
+####################
+## File-Operations ask before overwriting
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
 ## Color grep
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
